69ea87f3b0e2a3a20a21fe83302828bee687d912
James Mikko P. Recario <jamesmikkoprecario@iskolarngbayan.pup.edu.ph>
2024-01-26 02:21:36 +0800

    Added a for loop to print pattern

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -2,3 +2,8 @@
 # Print the following pattern
 
 # Use for loop to print pattern
+
+for number in range(1, 6):
+    for number_of_times_to_be_printed in range(number):
+        print(number, end=" ")
+    print("\n")
